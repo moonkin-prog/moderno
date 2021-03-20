16e6099ffd069ac0bd2345ee4f1a41928e8f8b7f
moonkin-prog <semeniuk-prog@gmail.com>
2021-03-20 22:15:08 +0200

    added section trends

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
+    
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
     var mixer = mixitup('.catalog');
+    
+    $('.trends__slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false,
+        dots: true
+    })
+
 });
