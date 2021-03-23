b272ef3f4271bf9e3422ba2c46ef034f757b14dd
moonkin-prog <semeniuk-prog@gmail.com>
2021-03-23 22:05:59 +0200

    adapted header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -5,14 +5,22 @@ $(function(){
         starWidth: "12px",
         readOnly: true
     });
-
-    var mixer = mixitup('.catalog');
     
     $('.trends__slider').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         arrows: false,
         dots: true
-    })
+    });
+
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').slideToggle()
+    });
+
+    $('.user-btn').on('click', function(){
+        $('.header__buttons').toggleClass('active');
+    });
+
+    var mixer = mixitup('.catalog');
 
 });
